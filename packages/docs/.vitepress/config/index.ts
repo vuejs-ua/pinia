50192a3012ea9d665aed18f6872b0a093e804d04
Serhii Matrunchyk <2089828+matrunchyk@users.noreply.github.com>
2023-09-26 08:45:11 +0300

    fix: Remove other languages

--- packages/docs/.vitepress/config/index.ts
+++ packages/docs/.vitepress/config/index.ts
@@ -1,34 +1,14 @@
 import { defineConfig } from 'vitepress'
-import { enConfig } from './en'
 import { sharedConfig } from './shared'
-import { zhConfig } from './zh'
 import { ukConfig } from './uk'
 
 export default defineConfig({
   ...sharedConfig,
 
   locales: {
-    root: { label: 'English', lang: 'en-US', link: '/', ...enConfig },
-    zh: { label: '简体中文', lang: 'zh-CN', link: '/zh/', ...zhConfig },
-    es: {
-      label: 'Español',
-      lang: 'es-ES',
-      link: 'https://es-pinia.vercel.app/',
-    },
-    ko: {
-      label: '한국어',
-      lang: 'ko-KR',
-      link: 'https://pinia.vuejs.kr/',
-    },
-    pt: {
-    	label: 'Português',
-    	lang: 'pt-PT',
-    	link: 'https://pinia-docs-pt.netlify.app/',
-    },
-    uk: {
-      label: 'Українська',
+    root: { label: 'Українська',
       lang: 'uk-UA',
-      link: 'https://pinia-ua.netlify.app/uk/',
+      link: '/',
       ...ukConfig
     },
   },
